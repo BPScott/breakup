068973ac40d636fb1c450504062c403b4c351b86
Ben Scott <ben@reload.me.uk>
2013-05-08 11:13:09 +0100

    Version bump to 0.2.0

--- lib/breakup.rb
+++ lib/breakup.rb
@@ -3,6 +3,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('breakup', :path => extension_path)
 
 module Breakup
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 end
 
